483711bf30e91ac54c1caa627debda4349c7c04d
Timothée Poux <timothee.poux@gmail.com>
2025-01-17 14:18:08 +0100

    Exemple1 pointeurs

--- TP/main.c
+++ TP/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 int main(){
+	/* EXEMPLE  CONST
+
 	const int a = 4;
 	a = 5;
+	*/
+
+	int a = 2;
+	printf("%p\n", &a);
+	int *p = &a;
+	printf("%p\n", p);
+	printf("%p\n", &p);
+	*p = 5;
+	printf("%d\n", a);
 
 	return 0;
 }
